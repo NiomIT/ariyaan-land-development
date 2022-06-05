4c148e230ce2b4f3f8dd031ad0426c405f4473d5
Moin <ourmoin@gmail.com>
2022-06-05 13:01:08 +0600

    up

--- data/about.js
+++ data/about.js
@@ -1,6 +1,6 @@
 const categoryData = [
     {
-        photo: "../img/sardar-palace.jpg",
+        photo: "../img/ariyaan-cover.jpg",
         about: "আপনার স্বপ্নের অ্যাপার্টমেন্টটি হোক সরদার প্যালেস-এ।"
     }
 ];
