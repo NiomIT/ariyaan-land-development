e358eaf9d6f383c4c728478ef49c7c3d353d7aaa
Moin <ourmoin@gmail.com>
2022-06-05 12:46:38 +0600

    update team

--- data/team.js
+++ data/team.js
@@ -1,18 +1,18 @@
 const categoryData = [
     {
-      photo: "https://scontent.fdac41-1.fna.fbcdn.net/v/t1.18169-9/12107039_136954893327349_7361340785337145390_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=174925&_nc_eui2=AeHxTMgHu20a_35lwnshbhn9nB0kDjQYF7GcHSQONBgXsbOgKaSB8mFE-GpP6FH4sZ-n-sJ7qR9dSp3M4LQyiZDK&_nc_ohc=Z-M3d19iHFwAX8vKwJY&_nc_ht=scontent.fdac41-1.fna&oh=00_AT_m16gWCJ8rZPy2EPEdj156QdcvichLedgXD2eQQel-ww&oe=62AE3C5C",
-      name: "মনিরুল আলম সেন্টু",
-      designation: "প্রধান উপদেষ্টা"
+      photo: "https://scontent.fdac41-1.fna.fbcdn.net/v/t1.18169-9/15740871_140115553148015_7188231699558111281_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=174925&_nc_eui2=AeHZp1zP3zCZreNWpWuCCMqdRfSILEr8rFNF9IgsSvysUx1SbQ53kTAAeCIJ_zt_I2rrNTvk9tjKMJZJEWoPnnKF&_nc_ohc=ik2M1-FoOUMAX8bz_dS&tn=DDI7aJ56NoH0PVNn&_nc_ht=scontent.fdac41-1.fna&oh=00_AT9Ms9Cz-6IyEFWu4oWrR01S0DlbPXLVkfIxrsIYk5BpFw&oe=62C16228",
+      name: "তানভীর আহমেদ",
+      designation: "চেয়ারম্যান"
     },
     {
-      photo: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/281803482_3302540959991936_8563885613916259844_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFocYwIlh4-TtXzyqsIK6DqsAwJWX8j-JOwDAlZfyP4k8pvqU9boMCoAzQA-r6qUyvHQ-LhqFygS8FdwUMTUv0S&_nc_ohc=3saXiscYVToAX_XXByl&tn=SGASXOK3c_jEIybK&_nc_ht=scontent.fdac41-1.fna&oh=00_AT8Ti8noTcCSqJTRH8NigwB9Q4legB6D1deaDUCIByrgAA&oe=628D1E84",
-      name: "মোস্তফা হোসেন চৌধুরী",
-      designation: "উপদেষ্টা"
+      photo: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/281362767_5654385631257564_5467133380677436508_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeG_yUHCMG8CT-3a_IjyhHQ6Yn7ATqDmP5RifsBOoOY_lI4gq8auPwDyxAYn4TY6A7p6GGWO1FLuVv72sVgu0U34&_nc_ohc=UOtWlN6IOEwAX8EuaUC&_nc_ht=scontent.fdac41-1.fna&oh=00_AT-eJhjCQiDLSjAmJybuB5T3D3Bo2vLyv7zdaGxmccoQCA&oe=62A10CDF",
+      name: "রাজন কুমার দাস",
+      designation: "একাউন্টেন্ট"
     },
     {
-      photo: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/281803482_3302540959991936_8563885613916259844_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFocYwIlh4-TtXzyqsIK6DqsAwJWX8j-JOwDAlZfyP4k8pvqU9boMCoAzQA-r6qUyvHQ-LhqFygS8FdwUMTUv0S&_nc_ohc=3saXiscYVToAX_XXByl&tn=SGASXOK3c_jEIybK&_nc_ht=scontent.fdac41-1.fna&oh=00_AT8Ti8noTcCSqJTRH8NigwB9Q4legB6D1deaDUCIByrgAA&oe=628D1E84",
-      name: "মোঃ মঞ্জুরুল ইসলাম",
-      designation: "উপদেষ্টা"
+      photo: "https://scontent.fdac41-1.fna.fbcdn.net/v/t1.6435-1/172135336_1833407730160708_4345048248900920865_n.jpg?stp=dst-jpg_p100x100&_nc_cat=103&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeHphoHLkBtjRFpeDM8xUoNxDFZQ1RuPx1YMVlDVG4_HVrptkG-3Xjh_bA8WWbPnRUO3rx5GMyZYG3lcBsUQywyq&_nc_ohc=fr88itQMHkMAX99xBe1&_nc_ht=scontent.fdac41-1.fna&oh=00_AT--nUADF4yhV_shoneMWHmOrs1gM0rtidWxcxNNjQj0OA&oe=62C00693",
+      name: "সায়েদ আহমেদ",
+      designation: "মার্কেটিং অফিসার"
     }
   ];
   
